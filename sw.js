b62a39eeb91c7b69dee4079a4fb9f02e43c3587f
dszenith <ds.thelegends@gmail.com>
2026-04-10 03:15:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'chatnith-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
